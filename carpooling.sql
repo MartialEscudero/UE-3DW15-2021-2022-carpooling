8f9af451a0585f102f1da46f6b5b0db699f26400
Escudero Martial <martial.escudero@gmail.com>
2021-11-22 19:21:45 +0100

    main: add .sql file

--- carpooling.sql
+++ carpooling.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Nov 22, 2021 at 02:39 PM
+-- Generation Time: Nov 22, 2021 at 06:21 PM
 -- Server version: 8.0.22
 -- PHP Version: 7.4.20
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `carpooling`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ads`
+--
+
+CREATE TABLE `ads` (
+  `id` int NOT NULL,
+  `price` int NOT NULL,
+  `placeStart` varchar(255) NOT NULL,
+  `placeEnd` varchar(255) NOT NULL,
+  `dateStart` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `ads`
+--
+
+INSERT INTO `ads` (`id`, `price`, `placeStart`, `placeEnd`, `dateStart`) VALUES
+(1, 50, 'Paris', 'Marseille', '2021-11-22 16:15:28'),
+(2, 30, 'Nantes', 'Angers', '2021-11-19 16:18:33'),
+(7, 60, 'Nice', 'Paris', '2021-12-10 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -67,7 +90,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
 (1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08 00:00:00'),
 (2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08 00:00:00'),
-(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08 00:00:00');
+(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08 00:00:00'),
+(4, 'Paul', 'Jean', 'sonmail@mail.fr', '2021-11-11 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -85,14 +109,20 @@ CREATE TABLE `users_cars` (
 --
 
 INSERT INTO `users_cars` (`user_id`, `car_id`) VALUES
-(1, 1),
 (1, 2),
 (2, 3),
-(3, 4);
+(3, 4),
+(4, 6);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cars`
@@ -117,6 +147,12 @@ ALTER TABLE `users_cars`
 --
 
 --
+-- AUTO_INCREMENT for table `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
@@ -126,7 +162,7 @@ ALTER TABLE `cars`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=369;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
