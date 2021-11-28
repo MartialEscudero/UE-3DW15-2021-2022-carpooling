18bbb2e0b2acc9a6af79e1e52806d38c0e23b293
Itspacome <contact@pacomeperrichot.com>
2021-11-28 22:08:10 +0100

    main: fix booking create

--- bookings_create.php
+++ bookings_create.php
@@ -18,13 +18,23 @@ $users = $usersService->getUsers();
 
 <p>Réservation</p>
 <form method="post" action="bookings_create.php" name="bookingCreateForm">
-    <label for="ad">Séléctionne une annonce :</label>
+    <label for="dayStart">Choisir une date au format dd-mm-yyyy :</label>
+    <input type="text" name="dayStart">
+    <br />
+    <label for="ad">Séléctionner une annonce :</label>
     <br />
     <?php foreach ($ads as $ad): ?>
-    <input type="radio" name="adId"
+    <input type="radio" name="ad_id"
         value="<?php echo $ad->getId(); ?>">
     <?php echo "Covoiturage de " . $ad->getPlaceStart() . " vers " . $ad->getPlaceEnd() . " le " . $ad->getDate()->format('d/m/Y'); ?>
     <br />
+    <?php endforeach; ?>
+    <br />
+    <label for="users">Covoitureur(s) :</label>
+    <?php foreach ($users as $user): ?>
+        <?php $userName = $user->getFirstname() . ' ' . $user->getLastname(); ?>
+        <input type="checkbox" name="users[]" value="<?php echo $user->getId(); ?>"><?php echo $userName; ?>
+        <br />
     <?php endforeach; ?>
     <br />
     <input type="submit" value="Réserver">
